84548cddba3d0f63b5c074d30c91a9d9c066ea66
Sena Ji <senasji@gmail.com>
2020-12-22 15:19:44 -0800

    Fully implemented Stack class

--- ruby_curriculum/sena_ji_diy_adts/exercises.rb
+++ ruby_curriculum/sena_ji_diy_adts/exercises.rb
@@ -1,17 +1,21 @@
 class Stack
   def initialize
     # create ivar to store stack here!
+    @stack = []
   end
 
   def push(el)
     # adds an element to the stack
+    @stack.push(el)
   end
 
   def pop
     # removes one element from the stack
+    @stack.pop
   end
 
   def peek
     # returns, but doesn't remove, the top element in the stack
+    @stack[0]
   end
 end
