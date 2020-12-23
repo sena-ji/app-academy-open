bd40c130ddc648f1b765599fcc3b54ed0f08384a
Sena Ji <senasji@gmail.com>
2020-12-22 17:19:00 -0800

    Implemented base for PolyTreeNode class

--- ruby_curriculum/polytreenode_project/lib/00_tree_node.rb
+++ ruby_curriculum/polytreenode_project/lib/00_tree_node.rb
@@ -1,3 +1,15 @@
 class PolyTreeNode
+  attr_reader :value, :parent, :children
 
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    @parent.children.delete(self) unless @parent.nil?
+    @parent = node
+    @parent.children << self unless node.nil?
+  end
 end
