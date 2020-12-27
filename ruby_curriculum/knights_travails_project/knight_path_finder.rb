0d7b9b10d844086ddd26ffe612a828ca69a78075
Sena Ji <senasji@gmail.com>
2020-12-26 18:29:46 -0800

    Added and tested out the test cases provided by App Academy

--- ruby_curriculum/knights_travails_project/knight_path_finder.rb
+++ ruby_curriculum/knights_travails_project/knight_path_finder.rb
@@ -66,3 +66,9 @@ class KnightPathFinder
     traced_nodes.reverse
   end
 end
+
+if __FILE__ == $PROGRAM_NAME
+  kpf = KnightPathFinder.new([0, 0])
+  p kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+  p kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
+end
