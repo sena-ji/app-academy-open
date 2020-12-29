31732fae156d53ad725a06903e1dbb2e8f33421c
Sena Ji <senasji@gmail.com>
2020-12-29 15:37:38 -0800

    Initialized both Employee and Manager classes

---
+++ ruby_curriculum/class_inheritance_project/employee.rb
@@ -0,0 +1,14 @@
+class Employee
+  attr_reader :name, :title, :salary, :boss
+
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+
+  def bonus(multiplier)
+    @salary * multiplier
+  end
+end
