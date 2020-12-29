28c06c82827bda223364e3ca8898d12e54d6247d
Sena Ji <senasji@gmail.com>
2020-12-29 15:12:02 -0800

    Updated user_script.rb to not raise any errors

--- ruby_curriculum/error_handling_funtime_project/user_script.rb
+++ ruby_curriculum/error_handling_funtime_project/user_script.rb
@@ -1,10 +1,13 @@
 require_relative 'super_useful'
 
-puts "'five' == #{convert_to_int('five')}"
+# not sure why this was here since the specs for this project did not include 
+# instructions on what to do with this section -> fixed it anyways!
+# problem was that the function takes in a integer that is a string and converts it to an integer
+puts "'five' == #{convert_to_int('5')}"
 
 feed_me_a_fruit
 
-sam = BestFriend.new('', 1, '')
+sam = BestFriend.new('Sam', 5, 'play some video games')
 
 sam.talk_about_friendship
 sam.do_friendstuff
