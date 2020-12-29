52993e1c3cda113a63e83280d782b7986610b4fd
Sena Ji <senasji@gmail.com>
2020-12-28 17:04:54 -0800

    Completed #place_stones and added method to #initialize

--- ruby_curriculum/mancala_homework/lib/board.rb
+++ ruby_curriculum/mancala_homework/lib/board.rb
@@ -4,10 +4,17 @@ class Board
   def initialize(name1, name2)
     @player1, @player2 = name1, name2
     @cups = Array.new(14) { Array.new }
+
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, idx|
+      if idx != 13 && idx != 6
+        4.times { cup << :stone }
+      end
+    end
   end
 
   def valid_move?(start_pos)
