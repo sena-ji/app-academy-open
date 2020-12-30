41e9f5f2e1a286d099957535f37c0e357c1b9e2b
Sena Ji <senasji@gmail.com>
2020-12-30 13:27:44 -0800

    Wrote necessary Board instance methods

--- ruby_curriculum/chess_project/board.rb
+++ ruby_curriculum/chess_project/board.rb
@@ -4,4 +4,16 @@ class Board
   def initialize
     @rows = Array.new(8) { Array.new(8) }
   end
+
+  def [](pos)
+
+  end
+
+  def []=(pos, val)
+
+  end
+
+  def move_piece(start_pos, end_pos)
+
+  end
 end
