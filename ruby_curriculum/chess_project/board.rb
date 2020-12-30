2811e408021e5cf87c822869c9fc3e993984728e
Sena Ji <senasji@gmail.com>
2020-12-30 14:21:17 -0800

    Implemented quick initialization of board

--- ruby_curriculum/chess_project/board.rb
+++ ruby_curriculum/chess_project/board.rb
@@ -2,7 +2,13 @@ class Board
   attr_reader :rows
 
   def initialize
-    @rows = Array.new(8) { Array.new(8) }
+    @rows = Array.new(8) { Array.new(8, nil) }
+
+    (0..1).each do |row|
+      (1..7).each do |col|
+        @rows[row][col] = Piece.new(white)
+      end
+    end
   end
 
   def [](pos)
--- ruby_curriculum/chess_project/piece.rb
+++ ruby_curriculum/chess_project/piece.rb
@@ -1,9 +1,7 @@
 class Piece
   attr_reader :color, :board, :pos
 
-  def initialize(color, board, pos)
+  def initialize(color)
     @color = :color
-    @board = board
-    @pos = pos
   end
 end
