656d27ee614a39c35e33e0786a1b93da6b9a9e5e
Sena Ji <senasji@gmail.com>
2020-12-30 15:47:53 -0800

    Added all methods needed for Slidable class

---
+++ ruby_curriculum/chess_project/slidable.rb
@@ -0,0 +1,25 @@
+module Slidable
+  HORIZONTAL_DIRS = Array
+  DIAGONAL_DIRS = Array
+
+  def horizontal_dirs
+
+  end
+
+  def diagonal_dirs
+
+  end
+
+  def moves
+
+  end
+
+  private
+  def move_dirs
+
+  end
+
+  def grow_unblocked_moves_in_dir(dx, dy)
+
+  end
+end
