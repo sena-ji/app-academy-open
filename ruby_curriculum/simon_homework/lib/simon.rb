5df515fb49d1819803131f021063b6136eece791
Sena Ji <senasji@gmail.com>
2020-12-30 18:23:53 -0800

    Finished simon project

--- ruby_curriculum/simon_homework/lib/simon.rb
+++ ruby_curriculum/simon_homework/lib/simon.rb
@@ -28,10 +28,38 @@ class Simon
 
   def show_sequence
     add_random_color
+
+    @seq.each do |color|
+      print color
+      sleep(1)
+      system 'clear'
+    end
   end
 
   def require_sequence
+    user_guess = []
+    guess_count = 0
 
+    puts "Enter a sequence of colors for a guess!" 
+
+    while true
+      print "> "
+      user_choice = gets.chomp
+      user_guess << user_choice
+      guess_count += 1
+
+      if user_guess == @seq
+        round_success_message
+        sleep(1)
+        system 'clear'
+        break
+      end
+
+      if user_guess.take(guess_count) != @seq.take(guess_count)
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
@@ -51,4 +79,9 @@ class Simon
     @game_over = false
     @sequence = []
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = Simon.new
+  game.play
 end
