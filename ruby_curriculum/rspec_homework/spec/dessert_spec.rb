adb876b78f213f0de0c9ddc339411cf94354f13c
Sena Ji <senasji@gmail.com>
2020-12-31 17:33:59 -0800

    Finished rspec portion for #initialize

--- ruby_curriculum/rspec_homework/spec/dessert_spec.rb
+++ ruby_curriculum/rspec_homework/spec/dessert_spec.rb
@@ -6,16 +6,18 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
+  subject(:ice_cream) { Dessert.new("ice cream", 3, "Steve")}
   let(:chef) { double("chef") }
 
   describe "#initialize" do
     it "sets a type"
-
+      expect(dessert.type).to eq('ice cream')
     it "sets a quantity"
-
+      expect(dessert.quantity).to eq(3)
     it "starts ingredients as an empty array"
-
+      expect(dessert.ingredients).to eq([])
     it "raises an argument error when given a non-integer quantity"
+      expect { Dessert.new("ice cream", "thirty", "Steve") }.to raise_error(ArgumentError)
   end
 
   describe "#add_ingredient" do
