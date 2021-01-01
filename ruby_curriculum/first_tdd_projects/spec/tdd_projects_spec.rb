5b31fd26b7071156260a02f9f398be5838563686
Sena Ji <senasji@gmail.com>
2021-01-01 15:37:36 -0800

    Added rspec for #two_sum

--- ruby_curriculum/first_tdd_projects/spec/tdd_projects_spec.rb
+++ ruby_curriculum/first_tdd_projects/spec/tdd_projects_spec.rb
@@ -7,7 +7,7 @@ clean and helpful cheatsheet to rspec that I found: https://devhints.io/rspec
 
 describe Array do
   describe "#my_uniq" do
-    let(:not_uniq) { [1, 2, 2, 3, 3, 3] }
+    let(:not_uniq) { [1, 2, 1, 3, 3] }
 
     it "removes duplicates from an array" do
       expect(not_uniq.my_uniq).to eq([1, 2, 3])
@@ -17,8 +17,20 @@ describe Array do
       expect(not_uniq.my_uniq).not_to eq([1, 3, 2])
       expect(not_uniq.my_uniq).not_to eq([2, 1, 3])
       expect(not_uniq.my_uniq).not_to eq([2, 3, 1])
-      expect(not_uniq.my_uniq).not_to eq([3, 2, 1])
       expect(not_uniq.my_uniq).not_to eq([3, 1, 2])
+      expect(not_uniq.my_uniq).not_to eq([3, 2, 1])
+    end
+  end
+
+  describe "#two_sum" do
+    let(:two_sum_arr) { [-1, 0, 2, -2, 1] }
+
+    it "finds all pairs of positions where the elements at positions sum to zero" do
+      expect(two_sum_arr.two_sum).to eq([[0, 4], [2, 3]])
+    end
+
+    it "should sort array of pairs dictionary-wise"
+      expect(two_sum_arr.two_sum).not_to eq([[4, 0], [3, 2]])
     end
   end
 end
