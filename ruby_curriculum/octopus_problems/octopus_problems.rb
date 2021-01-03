22de367f0df45f23fc5249e84268d5f75f71642f
Sena Ji <senasji@gmail.com>
2021-01-03 12:30:15 -0800

    Finished #dominant_octopus and utilized past sorting algorithm

--- ruby_curriculum/octopus_problems/octopus_problems.rb
+++ ruby_curriculum/octopus_problems/octopus_problems.rb
@@ -14,3 +14,45 @@ def sluggish_octopus(fish_array)
 
   longest_fish
 end
+
+# Find the longest fish in O(n log n) time.
+def dominant_octopus(fish_array)
+  return fish_array if fish_array.length <= 1
+
+  mid_fish = fish_array.length / 2
+  left_fishes_sorted = fish_array.take(fish_array.length/2)
+  right_fishes_sorted = fish_array.drop(fish_array.length/2)
+
+  merge_halves(left_fishes_sorted, right_fishes_sorted)
+end
+
+def merge_halves(left, right)
+  new_array = []
+  left_idx, right_idx = 0, 0
+
+  while left_idx < left.length && right_idx < right.length
+    case left[left_idx] <=> right[right_idx]
+    when -1
+      new_array << left[left_idx]
+      left_idx += 1
+    when 0
+      new_array << left[left_idx]
+      left_idx += 1
+    when 1
+      new_array << right[right_idx]
+      right_idx += 1
+    end
+  end
+
+  while left_idx < left.length
+    new_array << left[left_idx]
+    left_idx += 1
+  end
+
+  while right_idx < right.length
+    new_array << right[right_idx]
+    right_idx += 1
+  end
+
+  new_array
+end
