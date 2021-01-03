c2544f343c8594c713f9e9d04bf9afb3175843cb
Sena Ji <senasji@gmail.com>
2021-01-03 13:08:30 -0800

    Added forgotten main #dominant_octopus method and implemented #clever_octopus method

--- ruby_curriculum/octopus_problems/octopus_problems.rb
+++ ruby_curriculum/octopus_problems/octopus_problems.rb
@@ -15,18 +15,24 @@ def sluggish_octopus(fish_array)
   longest_fish
 end
 
-# Find the longest fish in O(n log n) time.
+# Find the longest fish in O(n log n) time. Hint: Use merge sort.
 def dominant_octopus(fish_array)
+  sorted_fishes = merge_sort_fishes(fish_array)
+
+  sorted_fishes.last
+end
+
+def merge_sort_fishes(fish_array)
   return fish_array if fish_array.length <= 1
 
   mid_fish = fish_array.length / 2
   left_fishes_sorted = fish_array.take(fish_array.length/2)
   right_fishes_sorted = fish_array.drop(fish_array.length/2)
 
-  merge_halves(left_fishes_sorted, right_fishes_sorted)
+  merge_fishes(left_fishes_sorted, right_fishes_sorted)
 end
 
-def merge_halves(left, right)
+def merge_fishes(left, right)
   new_array = []
   left_idx, right_idx = 0, 0
 
@@ -55,4 +61,15 @@ def merge_halves(left, right)
   end
 
   new_array
+end
+
+# Find the longest fish in O(n) time. The octopus can hold on to the longest fish while stepping through the array only once.
+def clever_octopus(fish_array)
+  longest_fish = fish_array.first
+
+  fish_array.each do |fish|
+    longest_fish = fish if longest_fish.length < fish.length
+  end
+
+  longest_fish
 end
