ad3e075ee21403aebb39957ab8aa7d34f94bdd30
Sena Ji <senasji@gmail.com>
2021-01-07 11:11:51 -0800

    Finished phase 1 for #my_min

--- ruby_curriculum/execution_time_differences_algorithm_project/execution_time_difference.rb
+++ ruby_curriculum/execution_time_differences_algorithm_project/execution_time_difference.rb
@@ -6,8 +6,11 @@
 # write a function that compares each element to every other element of the list.
 # return the element if all other elements in the array are larger
 def my_min1(int_list)
-
+  int_list.each do |int1|
+    return int1 if int_list.all? { |int2| int2 >= int1 }
+  end
 end
+# what is the time complexity? O(n^2)
 
 # phase 2
 # rewrite the function to iterate through the list just once while keeping track of the minimum
@@ -21,3 +24,7 @@ def my_min2(int_list)
   min
 end
 # what is the time complexity? O(n)
+
+# find the sums of all contiguous sub-arrays and return the max
+# list = [5, 3, -7]
+# largest_contiguous_subsum(list) # => 8
