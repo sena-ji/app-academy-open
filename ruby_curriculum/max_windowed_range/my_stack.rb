40ee9f0622de43e677e1d999dab800a66294ebc5
Sena Ji <senasji@gmail.com>
2021-01-12 14:24:17 -0800

    Finished MyStack class

--- ruby_curriculum/max_windowed_range/my_stack.rb
+++ ruby_curriculum/max_windowed_range/my_stack.rb
@@ -1,3 +1,25 @@
 class MyStack
+  def initialize
+    @store = []
+  end
 
+  def peek
+    @store[-1]
+  end
+
+  def size
+    @store.size
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+  def pop
+    @store.pop
+  end
+
+  def push(elem)
+    @store.push(elem)
+  end
 end
