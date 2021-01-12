55072f32958d124f161eade5e82ccffb10de9dc2
Sena Ji <senasji@gmail.com>
2021-01-12 11:52:25 -0800

    Finished implementing #okay_two_sum? with sorting method

--- ruby_curriculum/two_sum_project/two_sum.rb
+++ ruby_curriculum/two_sum_project/two_sum.rb
@@ -9,10 +9,24 @@ def bad_two_sum?(arr, target_sum)
   false
 end
 
+# time complexity is O(n) as indicated in this stackoverflow thread:
+# https://stackoverflow.com/questions/8334981/find-pair-of-numbers-in-array-that-add-to-given-sum
+def okay_two_sum?(arr, target_sum)
+  sorted_arr = arr.sort
+  left_pointer = 0
+  right_pointer = arr.length - 1
 
+  while left_pointer < right_pointer
+    return true if arr[left_pointer] + arr[right_pointer] == target_sum
+    left_pointer += 1 if arr[left_pointer] + arr[right_pointer] < target_sum
+    right_pointer -= 1 if arr[left_pointer] + arr[right_pointer] > target_sum
+  end
+
+  false
+end
 
 if __FILE__ == $PROGRAM_NAME
   arr = [0, 1, 5, 7]
-  puts bad_two_sum?(arr, 6) # => should be true
-  puts bad_two_sum?(arr, 10) # => should be false
+  puts okay_two_sum?(arr, 6) # => should be true
+  puts okay_two_sum?(arr, 10) # => should be false
 end
