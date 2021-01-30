0d4533348daad7978f92c06d259329b1f4de6fe7
Sena Ji <senasji@gmail.com>
2021-01-30 14:27:03 -0800

    Finished 04a bonus

--- sql/sql_zoo_project/lib/04a_countries_bonus.rb
+++ sql/sql_zoo_project/lib/04a_countries_bonus.rb
@@ -34,6 +34,21 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+    SELECT
+      countries.continent,
+      countries.name,
+      countries.area
+    FROM
+      countries
+    WHERE
+      countries.area IN (
+        SELECT
+          MAX(countries.area)
+        FROM
+          countries
+        GROUP BY
+          countries.continent
+      )
   SQL
 end
 
@@ -41,5 +56,20 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+    SELECT
+      countries1.name,
+      countries1.continent
+    FROM
+      countries AS countries1
+    WHERE
+      countries1.population > 3 * (
+        SELECT
+          MAX(countries2.population)
+        FROM
+          countries AS countries2
+        WHERE
+          (countries1.continent = countries2.continent
+            AND countries1.name != countries2.name)
+      )
   SQL
 end
