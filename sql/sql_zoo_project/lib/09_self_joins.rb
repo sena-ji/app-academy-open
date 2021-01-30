c6c94960cf620632e60cbe4fb7e018a9e608f84b
Sena Ji <senasji@gmail.com>
2021-01-29 20:19:52 -0800

    Solved #cl_to_lr

--- sql/sql_zoo_project/lib/09_self_joins.rb
+++ sql/sql_zoo_project/lib/09_self_joins.rb
@@ -107,6 +107,19 @@ def cl_to_lr
   # Craiglockhart, without changing routes. Change the query so that it
   # shows the services from Craiglockhart to London Road.
   execute(<<-SQL)
+    SELECT
+      a.company,
+      a.num,
+      a.stop_id,
+      b.stop_id
+    FROM
+      routes a
+    JOIN
+      routes b ON (a.company = b.company
+                    AND a.num = b.num)
+    WHERE
+      (a.stop_id = 53
+        AND b.stop_id = 149)
   SQL
 end
 
