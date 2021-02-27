13435fc1b73b3d21be89a2f4d491b677d5a06d7c
Sena Ji <senasji@gmail.com>
2021-02-26 16:32:50 -0800

    Added Reply::find_by_user_id

--- sql_curriculum/aa_questions_project/reply.rb
+++ sql_curriculum/aa_questions_project/reply.rb
@@ -21,6 +21,19 @@ class Reply
     Reply.new(reply.first)
   end
 
+  def self.find_by_user_id(user_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        user_id = ?
+    SQL
+
+    replies.map { |reply| Reply.new(reply) }
+  end
+
   def initialize(options)
     @id = options['id']
     @body = options['body']
